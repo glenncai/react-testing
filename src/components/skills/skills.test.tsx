f4b22935fb1bed47eeda11519d39cedd9a9cc929
Glenn Cai <glenn.lh.cai@gmail.com>
2023-11-04 18:47:59 +0800

    test: debugging with logRoles

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, logRoles } from '@testing-library/react';
 import Skills from './skills';
 
 describe('Skills', () => {
@@ -31,7 +31,8 @@ describe('Skills', () => {
   });
 
   test('starting learning button is eventually rendered', async () => {
-    render(<Skills skills={skills} />);
+    const view = render(<Skills skills={skills} />);
+    logRoles(view.container);
     const learningButton = await screen.findByRole(
       'button',
       {
